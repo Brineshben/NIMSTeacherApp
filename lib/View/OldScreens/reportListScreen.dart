66259ea57b0a1690c209378d5c942144bc6b8ed4
NithinNelson <nithinns2000@gmail.com>
2024-12-24 14:02:15 +0530

    bug fix

--- lib/View/OldScreens/reportListScreen.dart
+++ lib/View/OldScreens/reportListScreen.dart
@@ -81,7 +81,7 @@ class _ReportListViewState extends State<ReportListView> {
     final bdy = {
       "action": "getFeedbackTotalSummaryData",
       "token": userAuthController.schoolToken.value,
-      "employee_code": userAuthController.userData.value.userId
+      "employee_code": userAuthController.userData.value.employeeNo
     };
 
     log("the >>>>>>>>>>>>>>>>>>>>> $bdy");
