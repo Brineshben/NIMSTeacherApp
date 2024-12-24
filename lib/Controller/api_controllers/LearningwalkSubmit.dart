6f20d7976e9ff761ca2ded419b8cbe85ed060ff7
Brinesh Wilson <brinesh@docme.cloud>
2024-12-24 12:13:42 +0530

    ui

--- lib/Controller/api_controllers/LearningwalkSubmit.dart
+++ lib/Controller/api_controllers/LearningwalkSubmit.dart
@@ -5,6 +5,7 @@ import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import '../../Models/api_models/LearningwalkSubmit.dart';
 import '../../Services/api_services.dart';
 import '../../View/CWidgets/TeacherAppPopUps.dart';
+import '../../sqflite_db/LearningWalkDatabase/LwarningwalkDB.dart';
 
 class LearningwalksubmitController extends GetxController {
   RxBool isLoading = false.obs;
@@ -36,12 +37,27 @@ class LearningwalksubmitController extends GetxController {
 
       }
       else {
-        TeacherAppPopUps.submitFailed(
-            title: "Failed",
-            message:"Something went Wrong",
-            actionName: "Ok",
-            iconData: Icons.error_outline,
-            iconColor: Colors.red);
+        final dbHelper =
+        LearningWalkDB();
+        final result = await dbHelper
+            .insertLearningWalk(
+            data.toMap());
+        print("brineshDB${result}");
+        TeacherAppPopUps
+            .submitFailedThreeLearningBack(
+          title: "Success",
+          message:
+          "Learning Walk Added Successfully",
+          actionName: "Close",
+          iconData: Icons.done,
+          iconColor: Colors.green,
+        );
+        // TeacherAppPopUps.submitFailed(
+        //     title: "Failed",
+        //     message:"Something went Wrong",
+        //     actionName: "Ok",
+        //     iconData: Icons.error_outline,
+        //     iconColor: Colors.red);
       }
 
 
